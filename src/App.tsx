d8455c1a83609a760a5deb29f36be842a78cd139
dnfny <dfeeney5@gmail.com>
2022-04-04 19:14:52 -0400

    Making a mess

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Box, Divider } from "@material-ui/core";
+import { SafetyDivider, SafetyDividerTwoTone } from '@mui/icons-material';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          hi i'm dana
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Divider component = "h1" light/>
+        <h1>is this thing working</h1>
+        
       </header>
+      <Box bgcolor='red'>
+        <p>hi</p>
+      </Box> 
+      
     </div>
   );
 }
